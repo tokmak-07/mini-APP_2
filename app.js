1818558af67fd1db9453ef0b7d38fb9a38359b02
umar tokmak <umartokmak07@gmail.com>
2024-07-13 17:26:44 +0300

    node_modules

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
-import { Bot, } from "grammy";
-
+import express from "express"
 const bot = new Bot("7499269454:AAHXC8Alkk85gPC59SYehRRsa__BRBV2uT4");
 
-bot.command("start",async(ctx) => ctx.reply("Добро пожаловать в наш бот"))
+
